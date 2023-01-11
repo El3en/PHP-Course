e41a197e06b2eb34a1a4bbfa890af3bdb7541b73
OHOUD ALHEFEITI <36378728+El3en@users.noreply.github.com>
2023-01-11 17:50:25 +0300

    Add files via upload

---
+++ admin/layouts/footer.php
@@ -0,0 +1,53 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+  <strong>Copyright &copy; 2014-2021
+    <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 3.2.0
+  </div>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?= BASE_ADMIN_URL?>plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?= BASE_ADMIN_URL?>plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge("uibutton", $.ui.button);
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?= BASE_ADMIN_URL?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="<?= BASE_ADMIN_URL?>plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="<?= BASE_ADMIN_URL?>plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="<?= BASE_ADMIN_URL?>plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?= BASE_ADMIN_URL?>plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?= BASE_ADMIN_URL?>plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?= BASE_ADMIN_URL?>plugins/moment/moment.min.js"></script>
+<script src="<?= BASE_ADMIN_URL?>plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?= BASE_ADMIN_URL?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="<?= BASE_ADMIN_URL?>plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?= BASE_ADMIN_URL?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= BASE_ADMIN_URL?>dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?= BASE_ADMIN_URL?>dist/js/pages/dashboard.js"></script>
+</body>
+
+</html>
