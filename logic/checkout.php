417fae30f53dae94ace367de82e91f3610d04ecf
OHOUD ALHEFEITI <36378728+El3en@users.noreply.github.com>
2023-01-17 20:24:46 +0400

    Delete logic directory

--- logic/checkout.php
+++
@@ -1,8 +0,0 @@
-<?php
-function addOrder($first_name, $last_name, $email)
-{
-    $file = fopen('./data/orders.csv', 'a+');
-    $data = $first_name . ',' . $last_name . ',' . $email . "\n";
-    fwrite($file, $data);
-    fclose($file);
-}
