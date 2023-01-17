2f383e7743b9831ddcc9ff302d27a4b18fd1d0ea
OHOUD ALHEFEITI <36378728+El3en@users.noreply.github.com>
2023-01-17 20:24:18 +0400

    Delete js directory

--- js/home.js
+++
@@ -1,5 +0,0 @@
-const addSingleProductToCart = (product) => {
-    const products = JSON.parse(localStorage.getItem("products") || "[]");
-    products.push(product);
-    localStorage.setItem("products", JSON.stringify(products));
-  };
